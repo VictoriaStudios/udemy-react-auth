8bb3720a582d6382d3735035e7a9636ae8b4b0bf
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-03-27 18:08:58 -0500

    Further implemented RESTful post

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,4 +1,5 @@
 import { useState, useRef } from 'react';
+import { apiKey } from '../../utilities/apiKey';
 
 import classes from './AuthForm.module.css';
 
@@ -14,6 +15,35 @@ const AuthForm = () => {
     const email= emailInputRef.current.value
     const password= passwordInputRef.current.value
 
+    if (isLogin) {
+
+    }
+    else {
+      fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email:email,
+          password:password,
+          returnToken:true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then (res => {
+        if (res.ok) {
+          //
+        } else {
+          res.json().then (data => {
+            let errorMessage = 'Create User failed'
+            if (data && data.error && data.error.message) errorMessage = data.error.message
+            alert (errorMessage)
+          })
+        }
+      })
+    }
+
   }
 
   const switchAuthModeHandler = () => {
@@ -23,7 +53,7 @@ const AuthForm = () => {
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
           <input ref={emailInputRef} type='email' id='email' required />
